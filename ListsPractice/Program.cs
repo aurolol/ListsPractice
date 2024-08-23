9ba97280d03007f0dacfea407c36f4a14552816a
auro <auro@DESKTOP-AV8I9BT>
2024-08-23 22:28:55 +0300

    Add project files.

---
+++ ListsPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+
+        List<string> davetliler = new List<string>()
+        {
+            "Bülent Ersoy",
+            "Ajda Pekkan",
+            "Ebru Gündeş",
+            "Hadise",
+            "Hande Yener",
+            "Tarkan",
+            "Funda Arar",
+            "Demet Akalın"
+        };
+
+        Console.WriteLine("** Davetliler **");
+
+        for (int i = 0; i < davetliler.Count; i++)
+        {
+            Console.WriteLine(i+1 + "-" + davetliler[i]);
+        }
+
+    }
+
+
+
+
+}
